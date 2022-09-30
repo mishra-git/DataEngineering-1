83f5382e47dfe306f46caa4e62c0f72f076be95e
Surya Mishra <Surya_Mishra@carmax.com>
2022-09-30 13:37:44 -0400

    updating credentials as same in both scripts

--- etl.py
+++ etl.py
@@ -117,7 +117,7 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=postgres password=Ch0ndr0m0@123q")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
