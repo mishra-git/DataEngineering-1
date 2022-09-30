7906cf9db5f6b8523d7363ad89549890423ab802
Surya Mishra <Surya_Mishra@carmax.com>
2022-09-30 13:59:17 -0400

    reversing time pk as it voilates every record

--- sql_queries.py
+++ sql_queries.py
@@ -45,7 +45,7 @@ artist_table_create = ("""
                        """)
                        
 time_table_create = ("""
-                     CREATE TABLE IF NOT EXISTS time (start_time timestamp primary key not null, \
+                     CREATE TABLE IF NOT EXISTS time (start_time timestamp  not null, \
                      hour int, \
                      day int, \
                      week int,\
